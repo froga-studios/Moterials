acf49df2c3afbfe043734d07ab7e25f5cdcad903
kraken <lennyj2468+github@gmail.com>
2021-04-09 19:04:24 +0100

    Stuff

--- src/main/java/io/github/frogastudios/moterials/registry/MTBlocks.java
+++ src/main/java/io/github/frogastudios/moterials/registry/MTBlocks.java
@@ -1,8 +1,11 @@
 package io.github.frogastudios.moterials.registry;
 
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
 import static io.github.frogastudios.moterials.Moterials.MODID;
@@ -13,5 +16,6 @@ public class MTBlocks
     public static void rB()
     {
         Registry.register(Registry.BLOCK, new Identifier(MODID, "enderite_block"), ENDERITE_BLOCK);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "enderite_block"), new BlockItem(ENDERITE_BLOCK, new FabricItemSettings().group(ItemGroup.BUILDING_BLOCKS)));
     }
 }
--- src/main/java/io/github/frogastudios/moterials/registry/MTItems.java
+++ src/main/java/io/github/frogastudios/moterials/registry/MTItems.java
@@ -11,9 +11,13 @@ import static io.github.frogastudios.moterials.Moterials.MODID;
 public class MTItems
 {
     public static final Item ENDERITE_INGOT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item ELDER_FRAGMENT = new Item(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final Item ELDER_SHARD = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
     public static void rI()
     {
         Registry.register(Registry.ITEM, new Identifier(MODID, "enderite_ingot"), ENDERITE_INGOT);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "elder_fragment"), ELDER_FRAGMENT);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "elder_shard"), ELDER_SHARD);
     }
 }
